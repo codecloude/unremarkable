ea819b7385e83fe8102bd6787610250d88a872ae
ilya morozov <moroz2002ilya@gmail.com>
2024-04-24 20:54:31 +0700

    complete first task

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,32 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const excludedParams = ['groupId']
 
+const getDecoded = (encoded, translations) => {
+    const decoded = []
+    const decodedValues = {};
 
+    for (const item of encoded) {
+        const decodedItem = {}
 
-// console.log(decoded)
+        for (const key in item) {
+            if (key.toLowerCase().endsWith('id') && item[key] !== null && !excludedParams.includes(key)) {
+                const value = translations[item[key]];
+                if (!decodedValues[value]) {
+                    // Если значение еще не было расшифровано, расшифровываем его
+                    decodedValues[item[key]] = value;
+                    decodedItem[key] = value;
+                }
+            } else {
+                decodedItem[key] = item[key]
+            }
+        }
+        decoded.push(decodedItem)
+    }
+
+    console.log(decoded)
+    console.log(decodedValues)
+    return decoded
+}
+
+getDecoded(encoded, translations)
